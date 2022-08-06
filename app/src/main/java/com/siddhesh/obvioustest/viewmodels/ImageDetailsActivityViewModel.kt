7ed979e5bab1db4d2e6aae9a3f41f8b9557d57aa
siddhesh_chandorkar <siddhesh.chandorkar@paynearby.in>
2022-08-06 22:43:19 +0530

    * Data set to detail activity

--- app/src/main/java/com/siddhesh/obvioustest/viewmodels/ImageDetailsActivityViewModel.kt
+++ app/src/main/java/com/siddhesh/obvioustest/viewmodels/ImageDetailsActivityViewModel.kt
@@ -7,5 +7,7 @@ import com.siddhesh.commons.models.ImageDetailsModel
 
 class ImageDetailsActivityViewModel(val imageDetailsModel: ImageDetailsModel) : ViewModel() {
     val hdUrl = ObservableField(imageDetailsModel.hdURL)
-    val title = ObservableField(imageDetailsModel.hdURL)
+    val title = ObservableField(imageDetailsModel.title)
+    val date = ObservableField(imageDetailsModel.date)
+    val explanation = ObservableField(imageDetailsModel.explanation)
 }
